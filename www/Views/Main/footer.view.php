d69105dcc8dbd038f880a4a735dd7a3dcca30b69
melvin-crypto <melvin.pierre.mp@gmail.com>
2023-07-05 18:18:05 +0200

    Changement contenu footer

--- www/Views/Main/footer.view.php
+++ www/Views/Main/footer.view.php
@@ -1,13 +1,18 @@
 <footer>
-	<div class="container-footer">
-		<nav>
-			<p>Mentions</p>
-			<p>Conditions d’utilisation</p>
-			<p>Conditions générales de vente</p>
-			<small>
-				© 2023 Club, Inc. Tous droits réservés
-			</small>
-		</nav>
-	
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <ul class="list-inline">
+                    <li class="list-inline-item"><a href="#">Mentions</a></li>
+                    <li class="list-inline-item"><a href="#">Conditions d’utilisation</a></li>
+                    <li class="list-inline-item"><a href="#">Conditions générales de vente</a></li>
+                </ul>
+            </div>
+            <div class="col-md-6 text-md-right">
+                <small>
+                    © 2023 Club, Inc. Tous droits réservés
+                </small>
+            </div>
+        </div>
+    </div>
 </footer>
