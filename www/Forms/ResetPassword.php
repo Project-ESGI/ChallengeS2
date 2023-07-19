18fc995a941a0d69cebba58547e4a3275468e58d
Jacksoumm <jacksamdu12@outlook.fr>
2023-07-19 12:16:09 +0200

    ajout d'un reset password

---
+++ www/Forms/ResetPassword.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Forms;
+
+use App\Forms\Abstract\AForm;
+
+class ResetPassword extends AForm
+{
+
+    protected $method = "POST";
+    protected $errors = [];
+
+    public function getConfig($row = []): array
+    {
+        $inputs = [
+
+            "email" => [
+                "type" => "email",
+                "min" => 5,
+                "max" => 255,
+                "placeholder" => "email",
+                "confirm" => "email",
+                "error" => "Veuillez saisir un email valide.",
+                "value" => $row ? trim($row['email']) : ''
+            ],
+            "confirm_email" => [
+                "type" => "email",
+                "min" => 5,
+                "max" => 255,
+                "placeholder" => "Confirmation de l'email",
+                "error" => "Veuillez saisir un email valide.",
+                "confirm" => "email",
+            ],
+        ];
+        return [
+            "config" => [
+                "method" => $this->getMethod(),
+                "action" => "",
+                "enctype" => "",
+                "titre" => "Inscription",
+                "errors" => $this->getErrors(),
+                "submit" => "S'inscrire",
+            ],
+            "inputs" => $inputs
+        ];
+    }
+
+    public function addError(string $fieldName, string $errorMessage)
+    {
+        $this->errors[$fieldName] = $errorMessage;
+    }
+
+    public function hasErrors(): bool
+    {
+        return !empty($this->errors);
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors ?? [];
+    }
+}
