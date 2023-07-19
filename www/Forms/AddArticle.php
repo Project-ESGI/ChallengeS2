14f9d382223db827bbbda505b1666f5d713a76d9
melvin-crypto <melvin.pierre.mp@gmail.com>
2023-07-19 12:18:43 +0200

    Ajout ckeditor

---
+++ www/Forms/AddArticle.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Forms;
+
+use App\Forms\Abstract\AForm;
+
+class AddArticle extends AForm
+{
+
+    protected $method = "POST";
+    protected $errors = [];
+
+    public function getConfig($row = [], $maj = null): array
+    {
+        $categories = [
+            "match_entrainement" => "Match d'entraînement",
+            "exercice" => "Exercice",
+            "competition" => "Compétition",
+            "tournoi" => "Tournoi"
+        ];
+
+        $inputs = [
+            "title" => [
+                "type" => "text",
+                "placeholder" => "Titre",
+                "min" => 2,
+                "max" => 45,
+                "error" => "Veuillez saisir un titre valide.",
+                "value" => $row ? trim($row['title']) : ''
+            ],
+            "slug" => [
+                "type" => "text",
+                "placeholder" => "Slug",
+                "min" => 2,
+                "max" => 45,
+                "error" => "Veuillez saisir un slug valide.",
+                "value" => $row ? trim($row['slug']) : ''
+            ],
+            "category" => [
+                "type" => "select",
+                "options" => $categories,
+                "error" => "Veuillez sélectionner une catégorie valide.",
+                "value" => $row ? trim($row['category']) : ''
+            ],
+            "content" => [
+                "type" => "textarea",
+                "placeholder" => "Contenu",
+                "min" => 10,
+                "max" => 5000,
+                "error" => "Veuillez saisir un contenu valide.",
+                "value" => $row ? trim($row['content']) : ''
+            ],
+        ];
+
+        $submit = $maj ? "Modifier" : "Ajouter";
+
+        return [
+            "config" => [
+                "method" => $this->getMethod(),
+                "action" => "",
+                "enctype" => "",
+                "titre" => $submit." un article",
+                "errors" => $this->getErrors(),
+                "submit" => $submit,
+            ],
+            "inputs" => $inputs
+        ];
+    }
+
+
+    public function addError(string $fieldName, string $errorMessage)
+    {
+        $this->errors[$fieldName] = $errorMessage;
+    }
+
+    public function hasErrors(): bool
+    {
+        return !empty($this->errors);
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors ?? [];
+    }
+
+}
