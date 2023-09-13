9eb2fd9bfd23d7f05708688ec032b87c122597e4
melvin-crypto <melvin.pierre.mp@gmail.com>
2023-09-13 13:12:32 +0200

    Fix installer routes

--- www/Controllers/Security.php
+++ www/Controllers/Security.php
@@ -16,9 +16,13 @@ class Security
 {
     public function login(): void
     {
-        $form = new ConnectionUser();
-        $view = new View("Auth/connection", "connection");
-        $view->assign('form', $form->getConfig());
+        if (yaml_parse_file('application.yml')) {
+            $form = new ConnectionUser();
+            $view = new View("Auth/connection", "connection");
+            $view->assign('form', $form->getConfig());
+        } else {
+            AuthorizationHelper::redirectTo404();
+        }
     }
 
     public function check(): void
@@ -51,37 +55,41 @@ class Security
 
     public function register(): void
     {
-        $form = new Registration();
-        $view = new View("Auth/register", "inscription");
-        $view->assign('form', $form->getConfig());
+        if (yaml_parse_file('application.yml')) {
+            $form = new Registration();
+            $view = new View("Auth/register", "inscription");
+            $view->assign('form', $form->getConfig());
 
-        if ($form->isSubmit()) {
-            $user = new User();
-            $date = new \DateTime();
-            $formattedDate = $date->format('Y-m-d H:i:s');
+            if ($form->isSubmit()) {
+                $user = new User();
+                $date = new \DateTime();
+                $formattedDate = $date->format('Y-m-d H:i:s');
 
-            $error = Verificator::form($form->getConfig(), $_POST);
+                $error = Verificator::form($form->getConfig(), $_POST);
 
-            foreach ($_POST as $key => $value) {
-                $_POST[$key] = htmlspecialchars(strip_tags($value), ENT_QUOTES, 'UTF-8');
+                foreach ($_POST as $key => $value) {
+                    $_POST[$key] = htmlspecialchars(strip_tags($value), ENT_QUOTES, 'UTF-8');
+                }
+
+                foreach ($error as $e => $data) {
+                    $form->addError($e, $data);
+                }
+
+                $view->assign('form', $form->getConfig($_POST));
+                if (!$error) {
+                    $user->saveUser(null, $_POST['firstname'], $_POST['lastname'], $_POST['pseudo'], $_POST['email'], $_POST['password'], $_POST['country'], 'user', $formattedDate, $formattedDate);
+                    $_SESSION['email'] = $_POST['email'];
+
+                    $mailDescription = "Cher utilisateur,\n\nNous sommes ravis de vous compter parmi nous ! Votre inscription a été confirmée avec succès.\n\nMerci de faire partie de notre communauté. Vous pouvez maintenant accéder à toutes les fonctionnalités de notre site et profiter de nos services.\n\nSi vous avez des questions ou avez besoin d'aide, n'hésitez pas à nous contacter. Nous sommes toujours là pour vous aider.\n\nEncore une fois, bienvenue !\n\nCordialement,\nL'équipe de ufc sport";
+
+                    $mail = new Mail($_POST['email'], "Inscription réussie", $mailDescription);
+                    $mail->sendEmail();
+                    header('Location: /');
+                }
+                exit;
             }
-
-            foreach ($error as $e => $data) {
-                $form->addError($e, $data);
-            }
-
-            $view->assign('form', $form->getConfig($_POST));
-            if (!$error) {
-                $user->saveUser(null, $_POST['firstname'], $_POST['lastname'], $_POST['pseudo'], $_POST['email'], $_POST['password'], $_POST['country'], 'user', $formattedDate, $formattedDate);
-                $_SESSION['email'] = $_POST['email'];
-
-                $mailDescription = "Cher utilisateur,\n\nNous sommes ravis de vous compter parmi nous ! Votre inscription a été confirmée avec succès.\n\nMerci de faire partie de notre communauté. Vous pouvez maintenant accéder à toutes les fonctionnalités de notre site et profiter de nos services.\n\nSi vous avez des questions ou avez besoin d'aide, n'hésitez pas à nous contacter. Nous sommes toujours là pour vous aider.\n\nEncore une fois, bienvenue !\n\nCordialement,\nL'équipe de ufc sport";
-
-                $mail = new Mail($_POST['email'], "Inscription réussie", $mailDescription);
-                $mail->sendEmail();
-                header('Location: /');
-            }
-            exit;
+        } else {
+            AuthorizationHelper::redirectTo404();
         }
     }
 
